71dbde6cdd3b4e0fe322f28af27d42cffc359d38
Fresh! <francocalvino89@gmail.com>
2021-08-25 16:12:16 +0200

    cambios en el home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,18 @@
   <div class="all">
     <div class="home">
        <div class="textoBienvenida">
-         <div>
-        <p>Bienvenido al Portfolio de</p>
-        <p>Franco Calviño </p>
+         <div class="centro a">
+          <p class="bienvenido">Bienvenido </p> 
+          <p class="al">al Portfolio </p>
+          <p class="de">de</p>
+          <p class="fc">Franco Calviño </p>
         </div>
-        <div>
-        <p>Desarrollador </p>
-        <p>Front End</p>
+        <div class="centro b">
+      
+        </div>
+        <div class="centro c">
+          <p>Desarrollador </p>
+          <p>Front End</p>
         </div>
       </div>
       <img class="imgFranco" alt="Foto Franco Calviño" src="../assets/imgFranco.jpg">
@@ -37,6 +42,38 @@
   display: flex;
   justify-content: space-around;
   align-items: center;
+}
+.a {
+  height: 45%;
+  width: 25%;
+  margin-left: 7%;
+}
+.bienvenido{
+  text-align: left !important;
+  margin-left: 8%;
+}
+.de{
+  margin-right: 10%;
+}
+.fc{
+  margin-left:30%;
+  margin-top: 10%;
+  font-size: 1.5rem;
+  font-weight: 500;
+  height: 10%;
+  transition: text-decoration 5s, font-size 2s;
+}
+
+.fc:hover{
+  font-size: 1.7rem ;
+  
+}
+
+.c{
+  width: 25%;
+  font-size: 1.9rem;
+  font-weight: bold;
+  margin-right: 10%;
 }
 
 
